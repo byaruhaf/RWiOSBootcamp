aad22409dd9e7339122907129e0a8a388ad902b4
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-05-29 10:12:28 +0200

    Completed expected functions for RGB & HSB Color Picker

--- Week1/ColorSlider/ColorSlider/Extensions/UIColor.swift
+++ Week1/ColorSlider/ColorSlider/Extensions/UIColor.swift
@@ -7,48 +7,20 @@
 //
 import UIKit.UIColor
 
-extension UIColor {
 
-    // MARK: - Initialization
-    convenience init(hex: String, alpha: CGFloat = 1.0) {
-        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        hexFormatted = hexFormatted.replacingOccurrences(of: "#", with: "")
-
-        assert(hexFormatted.count == 6, "Invalid hex code used.")
-
-        var rgbValue: UInt64 = 0
-
-        // Create Scanner
-        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
-
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                  alpha: alpha)
-    }
-
-    // MARK: - Convenience Methods
-
-    var toHex: String? {
-        // Extract Components
-        guard let components = cgColor.components, components.count >= 3 else {
-            return nil
+    extension UIColor {
+        convenience init(rgbColorCodeRed red: Float, green: Float, blue: Float) {
+            let redPart: CGFloat = CGFloat(red) / 255
+            let greenPart: CGFloat = CGFloat(green) / 255
+            let bluePart: CGFloat = CGFloat(blue) / 255
+            let alpha: CGFloat = 1.0
+            self.init(red: redPart, green: greenPart, blue: bluePart, alpha: alpha)
         }
-
-        // Helpers
-        let r = Float(components[0])
-        let g = Float(components[1])
-        let b = Float(components[2])
-        var a = Float(1.0)
-
-        if components.count >= 4 {
-            a = Float(components[3])
+        convenience init(hsbColorCodeHue hue: Float, saturation: Float, brightness: Float) {
+            let hue: CGFloat = CGFloat(hue) / 360
+            let saturation: CGFloat = CGFloat(saturation) / 100
+            let brightness: CGFloat = CGFloat(brightness) / 100
+            let alpha: CGFloat = 1.0
+            self.init(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
         }
-
-        // Create Hex String
-        let hex = String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
-
-        return hex
     }
-
-}
--- Week1/ColorSlider/ColorSlider/ViewController.swift
+++ Week1/ColorSlider/ColorSlider/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: IBOutlet
     @IBOutlet weak var colorModelSelector: UISegmentedControl!
     @IBOutlet weak var colorname: UILabel!
     // Red or Hue IBOutlet's
@@ -23,10 +24,83 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderThree: UISlider!
     @IBOutlet weak var sliderThreeLable: UILabel!
     @IBOutlet weak var sliderThreeValue: UILabel!
+    @IBOutlet weak var colorView: UIView!
+
+    //color for colorView
+    var color: UIColor = .white
+    var isRGB:Bool = false
+    var isHSB:Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupSliders() 
+    }
+
+    private func setupSliders() {
+
+        switch colorModelSelector.selectedSegmentIndex {
+        case 0:
+            isRGB = true
+            isHSB = false
+            sliderOneLable.text = "Red"
+            sliderTwoLable.text = "Green"
+            sliderThreeLable.text = "Blue"
+            sliderOne.maximumValue = 255
+            sliderTwo.maximumValue = 255
+            sliderThree.maximumValue = 255
+
+        case 1:
+            isRGB = false
+            isHSB = true
+            sliderOneLable.text = "Hue"
+            sliderTwoLable.text = "Saturation"
+            sliderThreeLable.text = "Brightness"
+            sliderOne.maximumValue = 360
+            sliderTwo.maximumValue = 100
+            sliderThree.maximumValue = 100
+        default:
+            break;
+        }
+        self.sliderOneValue.text = "0"
+        self.sliderTwoValue.text = "0"
+        self.sliderThreeValue.text = "0"
+        // Configure Sliders
+        sliderOne.value = 0.0
+        sliderTwo.value = 0.0
+        sliderThree.value = 0.0
+    }
+
+
+// moving of Slider Updates Values displayed
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        self.sliderOneValue.text = "\(Int(sliderOne.value))"
+        self.sliderTwoValue.text = "\(Int(sliderTwo.value))"
+        self.sliderThreeValue.text = "\(Int(sliderThree.value))"
+    }
+
+
+    @IBAction func updateColor(_ sender: Any) {
+        if isRGB {
+            // Create Color using RGB
+            let color = UIColor(rgbColorCodeRed: sliderOne.value, green: sliderTwo.value, blue: sliderThree.value)
+
+
+            // Configure Color View
+            colorView.backgroundColor = color
+        }
+        if isHSB {
+            // Create Color using HSB
+            let color = UIColor(hsbColorCodeHue: sliderOne.value, saturation: sliderTwo.value, brightness: sliderThree.value)
+            // Configure Color View
+            colorView.backgroundColor = color
+        }
+    }
+    @IBAction func resetSelection(_ sender: Any) {
+        setupSliders()
+    }
+    @IBAction func modeChanged(_ sender: Any) {
+        setupSliders()
     }
 
 
