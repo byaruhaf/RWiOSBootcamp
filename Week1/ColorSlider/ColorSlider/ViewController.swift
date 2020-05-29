ef747d2711d5fdfcfb1458e2cd40d5eda1a5df35
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-05-29 06:41:34 +0200

    added ColorSlider app for week1 Assignment

---
+++ Week1/ColorSlider/ColorSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorSlider
+//
+//  Created by Franklin Byaruhanga on 29/05/2020.
+//  Copyright © 2020 Franklin Byaruhanga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
