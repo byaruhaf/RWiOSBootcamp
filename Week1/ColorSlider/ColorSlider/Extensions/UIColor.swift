8fb7f7ee465d24e830a0017982dee5578b61a72c
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-05-29 07:11:16 +0200

    Added views to the storyboard & an extension UIColor

---
+++ Week1/ColorSlider/ColorSlider/Extensions/UIColor.swift
@@ -0,0 +1,53 @@
+//
+//  UIColor.swift
+//  ColorSlider
+//
+//  Created by Franklin Byaruhanga on 29/05/2020.
+//  Copyright © 2020 Franklin Byaruhanga. All rights reserved.
+//
+
+extension UIColor {
+
+    // MARK: - Initialization
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        hexFormatted = hexFormatted.replacingOccurrences(of: "#", with: "")
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+
+        // Create Scanner
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+
+    // MARK: - Convenience Methods
+
+    var toHex: String? {
+        // Extract Components
+        guard let components = cgColor.components, components.count >= 3 else {
+            return nil
+        }
+
+        // Helpers
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        var a = Float(1.0)
+
+        if components.count >= 4 {
+            a = Float(components[3])
+        }
+
+        // Create Hex String
+        let hex = String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+
+        return hex
+    }
+
+}
