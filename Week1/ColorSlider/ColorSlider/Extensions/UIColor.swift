e76cabdc61688d9675cc43327772baa09c5732ca
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-05-30 16:37:25 +0200

    Added Default background colour and broken up View controller in to smaller extensions

--- Week1/ColorSlider/ColorSlider/Extensions/UIColor.swift
+++ Week1/ColorSlider/ColorSlider/Extensions/UIColor.swift
@@ -7,36 +7,33 @@
 //
 import UIKit.UIColor
 
-    extension UIColor {
-        convenience init(rgbColorCodeRed red: Float, green: Float, blue: Float) {
-            let redPart: CGFloat = CGFloat(red) / 255
-            let greenPart: CGFloat = CGFloat(green) / 255
-            let bluePart: CGFloat = CGFloat(blue) / 255
-            let alpha: CGFloat = 1.0
-            self.init(red: redPart, green: greenPart, blue: bluePart, alpha: alpha)
-        }
-        convenience init(hsbColorCodeHue hue: Float, saturation: Float, brightness: Float) {
-            let hue: CGFloat = CGFloat(hue) / 360
-            let saturation: CGFloat = CGFloat(saturation) / 100
-            let brightness: CGFloat = CGFloat(brightness) / 100
-            let alpha: CGFloat = 1.0
-            self.init(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
-        }
-        
-    }
-
+// initial background color of the app
 extension UIColor {
-    func maxBright() -> UIColor {
-        var r:CGFloat = 0.0; var g:CGFloat = 0.0; var b:CGFloat = 0.0; var a:CGFloat = 0.0;
-        if self.getRed(&r, green: &g, blue: &b, alpha: &a) {
-            let d:CGFloat = 1.0 - max(r,g,b)
-            return UIColor(red: r + d, green: g + d , blue: b + d, alpha: 1.0)
+    static let bitterSweet = UIColor(hsbColorCodeHue:154, saturation:59, brightness:25)
+}
 
-        }
-        return self
+// Addpating UIColor initializer to make it easier at the call site
+extension UIColor {
+    convenience init(rgbColorCodeRed red: Float, green: Float, blue: Float) {
+        let redPart: CGFloat = CGFloat(red) / 255
+        let greenPart: CGFloat = CGFloat(green) / 255
+        let bluePart: CGFloat = CGFloat(blue) / 255
+        let alpha: CGFloat = 1.0
+        self.init(red: redPart, green: greenPart, blue: bluePart, alpha: alpha)
+    }
+    convenience init(hsbColorCodeHue hue: Float, saturation: Float, brightness: Float) {
+        let hue: CGFloat = CGFloat(hue) / 360
+        let saturation: CGFloat = CGFloat(saturation) / 100
+        let brightness: CGFloat = CGFloat(brightness) / 100
+        let alpha: CGFloat = 1.0
+        self.init(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
     }
 
-    // Swift
+}
+
+// Addpating UIColor to provide an the reverse of the color selected by the user
+// so the text color in the lables can addapt to user color's
+extension UIColor {
     func inverseColor() -> UIColor {
         var alpha: CGFloat = 1.0
 
