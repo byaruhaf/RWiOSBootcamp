6b2d06e707c22e7b555105b309e6d6acbf0aa6af
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-06-21 15:33:44 +0200

    fixed edge case for missing 0% result
    Signed-off-by: Franklin Byaruhanga <byaruhaf@appbantu.com>

--- Week4/CompatibilitySlider/CompatibilitySliderTests/CompatibilitySlider_StartTests.swift
+++ Week4/CompatibilitySlider/CompatibilitySliderTests/CompatibilitySlider_StartTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 @testable import CompatibilitySlider
 var sut: ViewController!
+var sut2: ViewController!
 
 class CompatibilitySlider_StartTests: XCTestCase {
 
@@ -19,14 +20,28 @@ class CompatibilitySlider_StartTests: XCTestCase {
         sut.person2 = Person(id: 2, items: [:])
         sut.compatibilityItems = ["Cats", "Dogs", "Chamois"]
         sut.currentPerson =  sut.person1
-         sut.currentPerson?.items.updateValue(1, forKey: "Cats")
-         sut.currentPerson?.items.updateValue(1, forKey: "Dogs")
-         sut.currentPerson?.items.updateValue(1, forKey: "Chamois")
+        sut.currentPerson?.items.updateValue(1, forKey: "Cats")
+        sut.currentPerson?.items.updateValue(1, forKey: "Dogs")
+        sut.currentPerson?.items.updateValue(1, forKey: "Chamois")
+        sut.currentPerson =  sut.person2
+        sut.currentPerson?.items.updateValue(1, forKey: "Cats")
+        sut.currentPerson?.items.updateValue(1, forKey: "Dogs")
+        sut.currentPerson?.items.updateValue(1, forKey: "Chamois")
 
-         sut.currentPerson =  sut.person2
-         sut.currentPerson?.items.updateValue(1, forKey: "Cats")
-         sut.currentPerson?.items.updateValue(1, forKey: "Dogs")
-         sut.currentPerson?.items.updateValue(1, forKey: "Chamois")
+        sut2 = ViewController()
+        sut2.person1 = Person(id: 1, items: [:])
+        sut2.person2 = Person(id: 2, items: [:])
+        sut2.compatibilityItems = ["Cats", "Dogs", "Chamois"]
+        sut2.currentPerson =  sut2.person1
+        sut2.currentPerson?.items.updateValue(0, forKey: "Cats")
+        sut2.currentPerson?.items.updateValue(0, forKey: "Dogs")
+        sut2.currentPerson?.items.updateValue(0, forKey: "Chamois")
+
+        sut2.currentPerson =  sut2.person2
+        sut2.currentPerson?.items.updateValue(5, forKey: "Cats")
+        sut2.currentPerson?.items.updateValue(5, forKey: "Dogs")
+        sut2.currentPerson?.items.updateValue(5, forKey: "Chamois")
+
  }
 
     override func tearDownWithError() throws {
@@ -34,9 +49,10 @@ class CompatibilitySlider_StartTests: XCTestCase {
     }
 
     func testCompatibilityCalculation() throws {
-        let result = sut.calculateCompatibility()
-        XCTAssertEqual( "100.0%", result)   
-
+        let result1 = sut.calculateCompatibility()
+        let result2 = sut2.calculateCompatibility()
+        XCTAssertEqual( "100.0%", result1)
+        XCTAssertEqual( "0.0%", result2)
     }
 
 }
