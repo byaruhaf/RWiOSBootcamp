dc1c5c74b7eba2d5405302c33a2bd9e614c35f05
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-06-25 11:24:57 +0200

    added starter Project
    Signed-off-by: Franklin Byaruhanga <byaruhaf@appbantu.com>

---
+++ Week5/Birdie/Birdie/Model/ImagePostModel.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePostModel.swift
+//  Birdie
+//
+//  Created by Jay Strawn on 6/18/20.
+//  Copyright © 2020 Jay Strawn. All rights reserved.
+//
+
+import UIKit
+
+struct ImagePost: MediaPost {
+    var textBody: String?
+    var userName: String
+    var timestamp: Date
+    var image: UIImage
+}
+
+
