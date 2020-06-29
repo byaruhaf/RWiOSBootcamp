741bab29e7bb1eb023e5d5ff9e0a0c8996b5488a
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-06-29 08:49:19 +0200

    Completed Stretch Goals

--- Week5/Birdie/Birdie/ControllernCoordinator/ViewController.swift
+++ Week5/Birdie/Birdie/ControllernCoordinator/ViewController.swift
@@ -17,9 +17,16 @@ class ViewController: UIViewController,UITableViewDataSource{
         setUpTableView()
     }
 
+    @IBAction func didPressCreateTextPostButton(_ sender: Any) {
+        createPost()
+    }
+
+    @IBAction func didPressCreateImagePostButton(_ sender: Any) {
+        createPost(withImage:true)
+    }
+
     func setUpTableView() {
         // Set delegates, register custom cells, set up datasource, etc.
-        tableview.separatorStyle = .none
         tableview.dataSource = self
         let Textnib = UINib(nibName: "TextPostCell",bundle: nil)
         let Imagenib = UINib(nibName: "ImagePostCell",bundle: nil)
@@ -29,15 +36,6 @@ class ViewController: UIViewController,UITableViewDataSource{
         tableview.reloadData()
     }
 
-    @IBAction func didPressCreateTextPostButton(_ sender: Any) {
-        createPost()
-        self.tableview.reloadData()
-    }
-
-    @IBAction func didPressCreateImagePostButton(_ sender: Any) {
-        createPost(containsImage:true)
-        self.tableview.reloadData()
-    }
 }
 
 extension ViewController {
@@ -48,6 +46,11 @@ extension ViewController {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let mediaPost = MediaPostsHandler.shared.mediaPosts[indexPath.row]
         let cell = MediaPostsCellCoordinator.shared.configureCell(for: mediaPost, in: tableView)
+        cell.backgroundView = UIView(backgroundColor: .white)
+        cell.backgroundView?.addSeparator()
+
+        cell.selectedBackgroundView = UIView(backgroundColor: .lightGray)
+        cell.selectedBackgroundView?.addSeparator()
         return cell
     }
 
@@ -61,17 +64,31 @@ extension ViewController {
 }
 
 extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-    func createPost(containsImage:Bool = false) {
-        if containsImage {
-            let pickerController = UIImagePickerController()
-            pickerController.delegate = self
-            present(pickerController, animated: true)
+
+    func createPost(withImage:Bool = false) {
+        if withImage {
+            selectImageForPost()
         } else {
-            showAlert()
+            requestPostDetails()
         }
     }
 
-    func showAlert() {
+    fileprivate func selectImageForPost() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = false
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else { return }
+        dismiss(animated: true)
+        requestPostDetails(withImage: image)
+    }
+
+
+    func requestPostDetails(withImage:UIImage? = nil) {
         let alert = UIAlertController(title: "Create Post", message: "Whats the latest update", preferredStyle: UIAlertController.Style.alert)
 
         alert.addTextField { (textField) in
@@ -91,8 +108,14 @@ extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerD
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler:{ (UIAlertAction) in
             guard let username = alert.textFields?[0].text else { return }
             let textPost = alert.textFields?[1].text
-            let post = TextPost(textBody: textPost, userName: username, timestamp: Date())
-            MediaPostsHandler.shared.addTextPost(textPost: post)
+            if let imageForPost = withImage {
+                let post = ImagePost(textBody: textPost, userName: username, timestamp: Date(), image: imageForPost)
+                MediaPostsHandler.shared.addImagePost(imagePost: post)
+            } else {
+                let post = TextPost(textBody: textPost, userName: username, timestamp: Date())
+                MediaPostsHandler.shared.addTextPost(textPost: post)
+            }
+
               self.tableview.reloadData()
         }))
 
@@ -100,4 +123,21 @@ extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerD
     }
 
 
+}
+
+private extension UIView {
+    convenience init(backgroundColor: UIColor) {
+        self.init()
+        self.backgroundColor = backgroundColor
+    }
+
+    func addSeparator() {
+        let separatorHeight: CGFloat = 2
+        let frame = CGRect(x: 0, y: bounds.height - separatorHeight, width: bounds.width, height: separatorHeight)
+        let separator = UIView(frame: frame)
+        separator.backgroundColor = .gray
+        separator.autoresizingMask = [.flexibleTopMargin, .flexibleWidth]
+
+        addSubview(separator)
+    }
 }
