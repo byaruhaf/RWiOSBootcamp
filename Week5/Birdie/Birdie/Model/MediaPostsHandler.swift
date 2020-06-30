78ef3e0c4576fc9600691f9dfef0678b3e4b4296
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-06-30 03:19:35 +0200

    updated initial Data

--- Week5/Birdie/Birdie/Model/MediaPostsHandler.swift
+++ Week5/Birdie/Birdie/Model/MediaPostsHandler.swift
@@ -15,10 +15,10 @@ class MediaPostsHandler: NSObject {
     private override init() {}
 
     func getPosts() {
-        let imagePost1 = ImagePost(textBody: "I love debugging software!", userName: "Jay", timestamp: Date(timeIntervalSince1970: 10000), image: UIImage(named: "chop")!)
-        let imagePost2 = ImagePost(textBody: "Went to the Aquarium today :]", userName: "Audrey", timestamp: Date(timeIntervalSince1970: 30000), image: UIImage(named: "octopus")!)
-        let textPost1 = TextPost(textBody: "Hello World!", userName: "Bhagat", timestamp: Date(timeIntervalSince1970: 20000))
-        let textPost2 = TextPost(textBody: "This is my favorite social media app!", userName: "Jeff", timestamp: Date(timeIntervalSince1970: 40000))
+        let imagePost1 = ImagePost(textBody: "There's two hard problems in computer science: we only have one joke and it's not funny.", userName: "Jay", timestamp: Date(timeIntervalSince1970: 10000), image: UIImage(named: "chop")!)
+        let imagePost2 = ImagePost(textBody: "There are only two hard things in Computer Science: cache invalidation and naming things. Phil Karlton", userName: "Audrey", timestamp: Date(timeIntervalSince1970: 30000), image: UIImage(named: "octopus")!)
+        let textPost1 = TextPost(textBody: "There are only two hard problems in distributed systems:  2. Exactly-once delivery 1. Guaranteed order of messages 2. Exactly-once delivery", userName: "Bhagat", timestamp: Date(timeIntervalSince1970: 20000))
+        let textPost2 = TextPost(textBody: "What are 2 hardest problems in computer science?", userName: "Jeff", timestamp: Date(timeIntervalSince1970: 40000))
 
         mediaPosts = [imagePost1, imagePost2, textPost1, textPost2]
         mediaPosts = mediaPosts.sorted(by: { $0.timestamp > $1.timestamp })
