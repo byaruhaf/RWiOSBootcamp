cf13a912befb4e1d37df1483f85e7ea176422efe
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-07-06 04:23:06 +0200

    mead model Hashable

--- Week6/Pokedex/Pokedex/Model/Pokemon.swift
+++ Week6/Pokedex/Pokedex/Model/Pokemon.swift
@@ -33,11 +33,19 @@
 import Foundation
 
 struct Pokemon:Hashable {
-  var id:UUID = UUID()
+  var identifier:UUID = UUID()
   var pokemonID:Int
   var pokemonName:String
   var baseExp:Int
   var weight:Int
   var height:Int
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(identifier)
+  }
+  static func == (lhs:Pokemon, rhs:Pokemon) -> Bool {
+    return lhs.identifier == rhs.identifier
+  }
+
 }
 
