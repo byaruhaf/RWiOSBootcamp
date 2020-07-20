2c86fe4c2ab768d45f0fd1a0efb20e5f3f303701
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-07-20 10:54:14 +0200

    Re-wired  the filter functionality to use Core Data queries rather than filtering the data array directly

--- Week8/SandwichSaturation/SandwichSaturation/ViewControllers/SandwichViewController.swift
+++ Week8/SandwichSaturation/SandwichSaturation/ViewControllers/SandwichViewController.swift
@@ -10,225 +10,221 @@ import UIKit
 import CoreData
 
 protocol SandwichDataSource {
-  func saveSandwich(_: SandwichData)
+    func saveSandwich(_: SandwichData)
 }
 
-class SandwichViewController: UITableViewController, SandwichDataSource, NSFetchedResultsControllerDelegate {
-  let searchController = UISearchController(searchResultsController: nil)
-//  var sandwiches = [SandwichData]()
-//  var filteredSandwiches = [SandwichData]()
-
+class SandwichViewController: UITableViewController, SandwichDataSource {
+    let searchController = UISearchController(searchResultsController: nil)
+      var sandwiches = [SandwichModel]()
+      var filteredSandwiches = [SandwichModel]()
     private let appDelegate =  UIApplication.shared.delegate as! AppDelegate
     private let persistentContiner:NSPersistentContainer! = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
 
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    guard persistentContiner != nil else { fatalError("This view needs a persistent container.") }
-    let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(presentAddView(_:)))
-    navigationItem.rightBarButtonItem = addButton
-    
-    // Setup Search Controller
-    searchController.searchResultsUpdater = self
-    searchController.obscuresBackgroundDuringPresentation = false
-    searchController.searchBar.placeholder = "Filter Sandwiches"
-    navigationItem.searchController = searchController
-    definesPresentationContext = true
-    searchController.searchBar.scopeButtonTitles = SauceAmount.allCases.map { $0.rawValue }
-    searchController.searchBar.delegate = self
-    searchController.searchBar.selectedScopeButtonIndex = UserDefaults.standard.integer(forKey: "selectedindex")
-    self.seed()
-    self.loadSandwiches()
-  }
-
-    // MARK: -
-
-    private lazy var fetchedResultsController:NSFetchedResultsController<SandwichModel>  = {
-        // Create Fetch Request
-        let fetchRequest: NSFetchRequest<SandwichModel> = SandwichModel.fetchRequest()
-
-        // Configure Fetch Request
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(SandwichModel.name), ascending: false)]
-
-        // Create Fetched Results Controller
-        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.persistentContiner.viewContext, sectionNameKeyPath: nil, cacheName: nil)
-
-        // Configure Fetched Results Controller
-        fetchedResultsController.delegate = self
-
-        return fetchedResultsController
-    }()
-
-    // MARK: -
-
-    private var hasSandwichs: Bool {
-        guard let fetchedObjects = fetchedResultsController.fetchedObjects else { return false }
-        return fetchedObjects.count > 0
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
 
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard persistentContiner != nil else { fatalError("This view needs a persistent container.") }
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(presentAddView(_:)))
+        navigationItem.rightBarButtonItem = addButton
+
+        // Setup Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Filter Sandwiches"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        searchController.searchBar.scopeButtonTitles = SauceAmount.allCases.map { $0.rawValue }
+        searchController.searchBar.delegate = self
+        searchController.searchBar.selectedScopeButtonIndex = UserDefaults.standard.integer(forKey: "selectedindex")
+        self.seed()
+        self.loadSandwiches()
+    }
+
+    // MARK: -
+
+//    private lazy var fetchedResultsController:NSFetchedResultsController<SandwichModel>  = {
+//        // Create Fetch Request
+//        let fetchRequest: NSFetchRequest<SandwichModel> = SandwichModel.fetchRequest()
+//
+//        // Configure Fetch Request
+//        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(SandwichModel.name), ascending: false)]
+//
+//        // Create Fetched Results Controller
+//        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.persistentContiner.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+//
+//        // Configure Fetched Results Controller
+//        fetchedResultsController.delegate = self
+//
+//        return fetchedResultsController
+//    }()
+
+    // MARK: -
+
+//    private var hasSandwichs: Bool {
+//        guard let fetchedObjects = fetchedResultsController.fetchedObjects else { return false }
+//        return fetchedObjects.count > 0
+//    }
+
+
+    //    func fetchSandwiches() {
+    //        do {
+    //            try fetchedResultsController.performFetch()
+    //        } catch {
+    //            print("Unable to Persorm Fetch Reests")
+    //            print("\(error),\(error.localizedDescription)")
+    //        }
+    //    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewDidAppear(animated)
         UserDefaults.standard.set(searchController.searchBar.selectedScopeButtonIndex, forKey: "selectedindex")
     }
-  
-  func loadSandwiches() {
-//    let sandwichArray = Bundle.main.decode([SandwichData].self, from: "sandwiches.json")
-    fetchSandwiches()
-//    sandwiches.append(contentsOf: sandwichArray)
-  }
 
-    func fetchSandwiches() {
+    func loadSandwiches() {
+        sandwiches.removeAll()
         do {
-            try fetchedResultsController.performFetch()
+            // Fetch current Sauce Amount's
+            let sandwichArray = try SandwichModel.findAll(in: persistentContiner.viewContext)
+             sandwiches.append(contentsOf: sandwichArray)
         } catch {
-            print("Unable to Persorm Fetch Reests")
-            print("\(error),\(error.localizedDescription)")
+            print("Unable to Fetch current Sauce Amounts, (\(error))")
         }
     }
 
-  func saveSandwich(_ sandwich: SandwichData) {
-    let newSandwich = SandwichModel(context: persistentContiner.viewContext)
-    // Configure sandwich
-    newSandwich.name = sandwich.name
-    newSandwich.imageName = sandwich.imageName
-    // Fetch available Sauce Amount's
-    let context: NSManagedObjectContext = persistentContiner.viewContext
-    let request:NSFetchRequest<SauceAmountModel> = SauceAmountModel.fetchRequest()
-    let currentSauceAmounts = try! context.fetch(request)
+    func saveSandwich(_ sandwich: SandwichData) {
+        let newSandwich = SandwichModel(context: persistentContiner.viewContext)
+        // Configure sandwich
+        newSandwich.name = sandwich.name
+        newSandwich.imageName = sandwich.imageName
 
-    newSandwich.tosauceAmount = currentSauceAmounts.first {
-        $0.sauceAmountString == sandwich.sauceAmount.rawValue
+        do {
+            // Fetch current Sauce Amount's
+            let currentSauceAmounts = try SauceAmountModel.findAll(in: persistentContiner.viewContext)
+            newSandwich.tosauceAmount = currentSauceAmounts.first {
+                $0.sauceAmountString == sandwich.sauceAmount.rawValue
+            }
+        } catch {
+            print("Unable to Fetch current Sauce Amounts, (\(error))")
+        }
+        appDelegate.saveContext()
+        loadSandwiches()
+        tableView.reloadData()
     }
 
-//    saveFood()
-    appDelegate.saveContext()
-    fetchSandwiches()
-    tableView.reloadData()
-  }
-
-  @objc
-  func presentAddView(_ sender: Any) {
-    performSegue(withIdentifier: "AddSandwichSegue", sender: self)
-  }
-  
-  // MARK: - Search Controller
-  var isSearchBarEmpty: Bool {
-    return searchController.searchBar.text?.isEmpty ?? true
-  }
-  
-  func filterContentForSearchText(_ searchText: String,
-                                  sauceAmount: SauceAmount? = nil) {
-//    filteredSandwiches = sandwiches.filter { (sandwhich: SandwichData) -> Bool in
-//      let doesSauceAmountMatch = sauceAmount == .any || sandwhich.sauceAmount == sauceAmount
-//
-//      if isSearchBarEmpty {
-//        return doesSauceAmountMatch
-//      } else {
-//        return doesSauceAmountMatch && sandwhich.name.lowercased()
-//          .contains(searchText.lowercased())
-//      }
-//    }
-    tableView.reloadData()
-  }
-  
-  var isFiltering: Bool {
-    let searchBarScopeIsFiltering =
-      searchController.searchBar.selectedScopeButtonIndex != 0
-    return searchController.isActive &&
-      (!isSearchBarEmpty || searchBarScopeIsFiltering)
-  }
-  
-  // MARK: - Table View
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    guard let sections = fetchedResultsController.sections else { return 0}
-    return sections.count
-  }
-
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//    return isFiltering ? filteredSandwiches.count : sandwiches.count
-    guard let section = fetchedResultsController.sections?[section] else { return 0}
-    return section.numberOfObjects
-  }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-    // Dequeue Reusable Cell
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: "sandwichCell", for: indexPath) as? SandwichCell else {
-        fatalError("Unexpected Index Path")
+    @objc
+    func presentAddView(_ sender: Any) {
+        performSegue(withIdentifier: "AddSandwichSegue", sender: self)
     }
 
-    let sandwich = fetchedResultsController.object(at: indexPath)
+    // MARK: - Search Controller
+    var isSearchBarEmpty: Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+
+    func filterContentForSearchText(_ searchText: String, sauceAmount: SauceAmount? = nil) {
+        var tablePredicate: NSCompoundPredicate {
+            if  sauceAmount == .any {
+                let p0 = NSPredicate(format: "name CONTAINS[cd] %@", "\(searchText)")
+                return NSCompoundPredicate(andPredicateWithSubpredicates: [p0])
+            } else {
+                let p0 = NSPredicate(format: "tosauceAmount.sauceAmountString == %@", "\(sauceAmount!.rawValue)")
+                let p1 = NSPredicate(format: "name CONTAINS[cd] %@", "\(searchText)")
+
+                return NSCompoundPredicate(andPredicateWithSubpredicates: [p0,p1])
+            }
+        }
+        filteredSandwiches.removeAll()
+        do {
+            // Fetch current Sauce Amount's
+            let sandwichArray = try SandwichModel.findFilterd(in: persistentContiner.viewContext, compoundPredicate: tablePredicate)
+            filteredSandwiches.append(contentsOf: sandwichArray)
+        } catch {
+            print("Unable to Fetch current Sauce Amounts, (\(error))")
+        }
+        tableView.reloadData()
+    }
+
+    var isFiltering: Bool {
+        let searchBarScopeIsFiltering =
+            searchController.searchBar.selectedScopeButtonIndex != 0
+        return searchController.isActive &&
+            (!isSearchBarEmpty || searchBarScopeIsFiltering)
+    }
+
+    // MARK: - Table View
+    override func numberOfSections(in tableView: UITableView) -> Int {
+//        guard let sections = fetchedResultsController.sections else { return 0}
+//        return sections.count
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return isFiltering ? filteredSandwiches.count : sandwiches.count
+//        guard let section = fetchedResultsController.sections?[section] else { return 0}
+//        return section.numberOfObjects
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // Dequeue Reusable Cell
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "sandwichCell", for: indexPath) as? SandwichCell else {
+//            fatalError("Unexpected Index Path")
+//        }
+
+//        let sandwich = fetchedResultsController.object(at: indexPath)
 
 
 
-//    guard let cell = tableView.dequeueReusableCell(withIdentifier: "sandwichCell", for: indexPath) as? SandwichCell
-//      else { return UITableViewCell() }
-    
-//    let sandwich = isFiltering ?
-//      filteredSandwiches[indexPath.row] :
-//      sandwiches[indexPath.row]
-
-    cell.thumbnail.image = UIImage.init(imageLiteralResourceName: sandwich.imageName!)
-    cell.nameLabel.text = sandwich.name
-    cell.sauceLabel.text = sandwich.tosauceAmount?.sauceAmount.rawValue
-
-    return cell
-  }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "sandwichCell", for: indexPath) as? SandwichCell
+              else { return UITableViewCell() }
+        let sandwich = isFiltering ? filteredSandwiches[indexPath.row] : sandwiches[indexPath.row]
+        cell.thumbnail.image = UIImage.init(imageLiteralResourceName: sandwich.imageName!)
+        cell.nameLabel.text = sandwich.name
+        cell.sauceLabel.text = sandwich.tosauceAmount?.sauceAmount.rawValue
+        return cell
+    }
 }
 
 // MARK: - UISearchResultsUpdating
 extension SandwichViewController: UISearchResultsUpdating {
-  func updateSearchResults(for searchController: UISearchController) {
-    let searchBar = searchController.searchBar
-    let sauceAmount = SauceAmount(rawValue:
-      searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex])
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        let sauceAmount = SauceAmount(rawValue:
+            searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex])
 
-    filterContentForSearchText(searchBar.text!, sauceAmount: sauceAmount)
-  }
+        filterContentForSearchText(searchBar.text!, sauceAmount: sauceAmount)
+    }
 }
 
 // MARK: - UISearchBarDelegate
 extension SandwichViewController: UISearchBarDelegate {
-  func searchBar(_ searchBar: UISearchBar,
-      selectedScopeButtonIndexDidChange selectedScope: Int) {
-    UserDefaults.standard.set(searchController.searchBar.selectedScopeButtonIndex, forKey: "selectedindex")
-    let sauceAmount = SauceAmount(rawValue:
-      searchBar.scopeButtonTitles![selectedScope])
-    filterContentForSearchText(searchBar.text!, sauceAmount: sauceAmount)
-  }
+    func searchBar(_ searchBar: UISearchBar,
+                   selectedScopeButtonIndexDidChange selectedScope: Int) {
+        UserDefaults.standard.set(searchController.searchBar.selectedScopeButtonIndex, forKey: "selectedindex")
+        let sauceAmount = SauceAmount(rawValue:
+            searchBar.scopeButtonTitles![selectedScope])
+        filterContentForSearchText(searchBar.text!, sauceAmount: sauceAmount)
+    }
 }
 
 
 
 extension SandwichViewController {
-
-//    fileprivate func saveFood() {
-//        do {
-//            // Save Changes
-//            print("Saving Changes \(persistentContiner)")
-//            try persistentContiner.viewContext.save()
-//        } catch {
-//            print("Unable to Save Main Managed Object Context After Seeding Persistent Store (\(error))")
-//        }
-//    }
-
     func seed() {
         guard !UserDefaults.didSeedPersistentStore else { return }
         var sauceAmountsBuffer: [SauceAmountModel] = []
-
         for amount in sauceAmounts {
             let sauceAmount = SauceAmountModel(context: persistentContiner.viewContext)
             sauceAmount.sauceAmountString = amount
             sauceAmountsBuffer.append(sauceAmount)
         }
-
         for data in sandwichArray {
             // Initialize sandwich
             let sandwich = SandwichModel(context: persistentContiner.viewContext)
@@ -240,17 +236,16 @@ extension SandwichViewController {
                 return $0.sauceAmountString == data.sauceAmount.rawValue
             }
         }
-
-//        saveFood()
         appDelegate.saveContext()
-
         // Update User Defaults
         UserDefaults.setDidSeedPersistentStore(true)
     }
 
     // Mark: - Seed Data
+    //Enum Data
     private var sauceAmounts: [String] {
         return SauceAmount.allCases.map { $0.rawValue }
     }
+    //JSON Data
     private var sandwichArray:[SandwichData] { Bundle.main.decode([SandwichData].self, from: "sandwiches.json")}
 }
