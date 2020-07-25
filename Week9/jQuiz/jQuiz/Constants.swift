4d368f706cfe60d50cfbc9691525685e96e0cde5
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-07-25 09:12:48 +0200

    Added Constants file to keep track of important app String

---
+++ Week9/jQuiz/jQuiz/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  jQuiz
+//
+//  Created by Franklin Byaruhanga on 25/07/2020.
+//  Copyright © 2020 Jay Strawn. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    struct Keys {
+        static let soundKey = "com.AppBantu.Sound.enabled"
+        static let iamgeKey = "logoImage"
+    }
+
+    struct URL {
+        static let imageURL = "https://cdn.dribbble.com/users/1405795/screenshots/4801691/1.jpg"
+        static let apiURL = "www.jservice.io"
+    }
+}
