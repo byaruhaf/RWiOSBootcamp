bb8b26cce3937d6d5a57905dd7d2189826282e7f
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-07-29 04:50:00 +0200

    Add new Constants

--- Week9/jQuiz/jQuiz/Constants/Constants.swift
+++ Week9/jQuiz/jQuiz/Constants/Constants.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit.UIColor
 
 struct K {
     struct Keys {
@@ -18,6 +19,13 @@ struct K {
         static let imageURL = "https://cdn.dribbble.com/users/1405795/screenshots/4801691/1.jpg"
         static let apiURL = "www.jservice.io"
     }
+
+    struct Colours {
+        static let c1 = UIColor(hue:0.704, saturation:0.884, brightness:0.719, alpha:1.000)
+        static let c2 = UIColor(hue:0.762, saturation:0.667, brightness:0.800, alpha:1.000)
+    }
+
+
 }
 
 struct CellType {
