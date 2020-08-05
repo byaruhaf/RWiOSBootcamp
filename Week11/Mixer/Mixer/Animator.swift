bc9b6b2e4b894083b6dc373ae5df93ffbad33732
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-08-05 22:35:34 +0200

    Basic App Working Adding and removing animations

---
+++ Week11/Mixer/Mixer/Animator.swift
@@ -0,0 +1,68 @@
+//
+//  Animator.swift
+//  Mixer
+//
+//  Created by Franklin Byaruhanga on 05/08/2020.
+//  Copyright © 2020 Franklin Byaruhanga. All rights reserved.
+//
+
+import UIKit
+
+public struct Animator {
+    public let duration: TimeInterval
+    public let closure: (UIView) -> Void
+}
+
+public extension Animator {
+    static func fadeIn(duration: TimeInterval = 3) -> Animator {
+        return Animator(duration: duration, closure: { $0.alpha = 1 })
+    }
+
+    static func resize(to size: CGSize, duration: TimeInterval = 3) -> Animator {
+        return Animator(duration: duration, closure: { $0.bounds.size = size })
+    }
+
+    static func move(duration: TimeInterval = 3) -> Animator {
+        return Animator(duration: duration, closure: {  $0.center = CGPoint(x: 150, y: 150) })
+    }
+
+    static func rotate(duration: TimeInterval = 3) -> Animator {
+        return Animator(duration: duration, closure: {   $0.transform = CGAffineTransform(rotationAngle: .pi/4) })
+    }
+
+    static func colorChange(duration: TimeInterval = 3) -> Animator {
+        return Animator(duration: duration, closure: {   $0.backgroundColor = UIColor.blue })
+    }
+}
+
+
+public extension UIView {
+    func perform(_ animations: [Animator]) {
+        // Exit condition: once all animations have been performed, we can return
+        guard !animations.isEmpty else {
+            return
+        }
+
+        // Remove the first animation from the queue
+        var animations = animations
+        let animation = animations.removeFirst()
+
+        // Perform the animation by calling its closure
+        UIView.animate(withDuration: animation.duration, animations: {
+            animation.closure(self)
+        }, completion: { _ in
+            // Recursively call the method, to perform each animation in sequence
+            self.perform(animations)
+        })
+    }
+}
+
+public extension UIView {
+    func performInParallel(_ animations: [Animator]) {
+        for animation in animations {
+            UIView.animate(withDuration: animation.duration) {
+                animation.closure(self)
+            }
+        }
+    }
+}
--- Week11/Mixer/Mixer/ViewController.swift
+++ Week11/Mixer/Mixer/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var stageview: UIView!
+    var viewAnimations:[Animator] = []
+
+    // A yellow box
+    let playBox = UIView(frame: CGRect(x: 10, y: 10, width: 40, height: 40))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stageview.backgroundColor = UIColor.red
+        playBox.backgroundColor = UIColor.yellow
+        stageview.addSubview(playBox)
+
+    }
+
+    func performMix() {
+        playBox.performInParallel(viewAnimations)
+    }
+
+    func resetMix() {
+        viewAnimations.removeAll()
+        viewAnimations.append(.resize(to: CGSize(width: 40, height: 40)))
+        playBox.performInParallel(viewAnimations)
     }
 
 
+    @IBAction func mixAnimation(_ sender: Any) {
+        performMix()
+    }
+
+    @IBAction func addAnimation1(_ sender: Any) {
+        viewAnimations.append(.fadeIn())
+        viewAnimations.append(.colorChange())
+    }
+
+    @IBAction func addAnimation2(_ sender: Any) {
+        viewAnimations.append(.resize(to: CGSize(width: 200, height: 200)))
+        viewAnimations.append(.move())
+    }
+
+    @IBAction func addAnimation3(_ sender: Any) {
+        resetMix()
+    }
 }
 
