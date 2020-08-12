d7acd114141db82dd962dcdb59f10986cd1a645e
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-08-12 15:15:14 +0200

    MenuButtonCompleted

--- Week11/Mixer/Mixer/ViewController.swift
+++ Week11/Mixer/Mixer/ViewController.swift
@@ -9,28 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var playPauseBtn: UIButton!
+    @IBOutlet weak var rotateBtn: UIButton!
+    @IBOutlet weak var resizeBtn: UIButton!
+    @IBOutlet weak var moveBtn: UIButton!
+    @IBOutlet weak var rotateBtnLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var resizeBtnBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var moveBtnTrailingConstraint: NSLayoutConstraint!
+    private var isMenueShowing = false
 
-    //animation object instantiated
-    var highScoreAnimation = HighScoreAnimation()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        animationSetup()
-         self.view.layer.addSublayer(highScoreAnimation.emitter)
+        playPauseBtn.layer.masksToBounds = true
+        playPauseBtn.layer.cornerRadius = playPauseBtn.frame.width/2
+        rotateBtn.layer.masksToBounds = true
+        rotateBtn.layer.cornerRadius = rotateBtn.frame.width/2
+        resizeBtn.layer.masksToBounds = true
+        resizeBtn.layer.cornerRadius = resizeBtn.frame.width/2
+        moveBtn.layer.masksToBounds = true
+        moveBtn.layer.cornerRadius = moveBtn.frame.width/2
+        isMenueShowing = false
+        menucontroller()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.highScoreAnimation.emitter.removeFromSuperlayer()
     }
 
-    // Animaition setup for Highscore's
-    fileprivate func animationSetup() {
-        highScoreAnimation.emitter.emitterPosition = CGPoint(x: self.view.frame.size.width / 2, y: -10)
-        highScoreAnimation.emitter.emitterShape = CAEmitterLayerEmitterShape.line
-        highScoreAnimation.emitter.emitterSize = CGSize(width: self.view.frame.size.width, height: 2.0)
-        highScoreAnimation.emitter.emitterCells = highScoreAnimation.generateEmitterCells()
+    @IBAction func playPauseTapped(_ sender: Any) {
+        isMenueShowing.toggle()
+
+        UIView.animate(withDuration: 0.5) {
+            self.menucontroller()
+            self.view.layoutIfNeeded()
+        }
     }
+
+    private func menucontroller() {
+        self.rotateBtnLeadingConstraint.constant = self.isMenueShowing ? -50 : 50
+        self.resizeBtnBottomConstraint.constant = self.isMenueShowing ? -50 : 50
+        self.moveBtnTrailingConstraint.constant = self.isMenueShowing ? -50 : 50
+    }
+
 
 }
